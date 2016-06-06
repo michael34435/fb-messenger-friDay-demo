d9a630af059fe52da801d0f0b58bb91b3ac63805
michael34435 <michael34435@gmail.com>
2016-06-07 06:40:45 +0800

    test

--- demo.js
+++ demo.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send(req.query);
+  if (req.query['hub.verify_token'] === 'here_friday_token') {
+    res.send(req.query['hub.challenge']);
+  }
 });
 
 app.listen(3000);
